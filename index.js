b090f2553a9cfcba40b0c9d7d7361e4d33f735fc
Ryan Conley <ryandeanconley2@yahoo.com>
2020-11-23 16:27:37 -0600

    completion

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const baseURL = 'https://dog.ceo/api/breed/';
+
+function getNumber() {
+  $('#number').submit(function (event) {
+    event.preventDefault();
+    let breed = $(event.currentTarget).find('input').val();
+    console.log(breed);
+
+
+
+    let request = `${baseURL}${breed}/images/random`;
+
+    console.log(request);
+
+
+
+    fetch(request).then((response) => response.json()).then((json) => {
+      let html = generateImages(json);
+      $('.images').html(html);
+    })
+      .catch(err => console.log(err));
+  });
+}
+
+function generateImages(json) {
+  let jsonArray = [];
+  //for (let i = 0; i < json.message.length; i++){
+  jsonArray.push(`<img src="${json.message}">`);
+  //  }
+  return jsonArray;
+}
+
+function main() {
+  getNumber();
+}
+
+$(main);
